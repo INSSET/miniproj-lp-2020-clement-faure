a43c262b99f71f86e3c3c9ed8ba98b42b7f69805
Clément Faure <clement.faure@etudiant.univ-reims.fr>
2020-12-11 14:14:24 +0100

    Ajout des entités

---
+++ src/Entity/Article.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article", indexes={@ORM\Index(name="Article_Utilisateur_FK", columns={"id_user", "email"}), @ORM\Index(name="IDX_23A0E66E7927C746B3CA4B", columns={"email", "id_user"})})
+ * @ORM\Entity
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_article", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idArticle;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="published", type="boolean", nullable=false)
+     */
+    private $published;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=50, nullable=false, options={"default"="#"})
+     */
+    private $title = '#';
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creation_date", type="date", nullable=false)
+     */
+    private $creationDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="publication_date", type="date", nullable=false)
+     */
+    private $publicationDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", length=65535, nullable=false)
+     */
+    private $content;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="email", referencedColumnName="email"),
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id_user")
+     * })
+     */
+    private $email;
+
+    public function getIdArticle(): ?int
+    {
+        return $this->idArticle;
+    }
+
+    public function getPublished(): ?bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): self
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(\DateTimeInterface $publicationDate): self
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getEmail(): ?User
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?User $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/Comment.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Comment
+ *
+ * @ORM\Table(name="comment", indexes={@ORM\Index(name="COMMENTAIRE_Article_FK", columns={"id_article"}), @ORM\Index(name="COMMENTAIRE_Utilisateur0_FK", columns={"id_user", "email"}), @ORM\Index(name="IDX_9474526CE7927C746B3CA4B", columns={"email", "id_user"})})
+ * @ORM\Entity
+ */
+class Comment
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_comment", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idComment;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text", length=65535, nullable=false)
+     */
+    private $content;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="creation_date", type="date", nullable=false)
+     */
+    private $creationDate;
+
+    /**
+     * @var \Article
+     *
+     * @ORM\ManyToOne(targetEntity="Article")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_article", referencedColumnName="id_article")
+     * })
+     */
+    private $idArticle;
+
+    /**
+     * @var \User
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="email", referencedColumnName="email"),
+     *   @ORM\JoinColumn(name="id_user", referencedColumnName="id_user")
+     * })
+     */
+    private $email;
+
+    public function getIdComment(): ?int
+    {
+        return $this->idComment;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creationDate;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creationDate): self
+    {
+        $this->creationDate = $creationDate;
+
+        return $this;
+    }
+
+    public function getIdArticle(): ?Article
+    {
+        return $this->idArticle;
+    }
+
+    public function setIdArticle(?Article $idArticle): self
+    {
+        $this->idArticle = $idArticle;
+
+        return $this;
+    }
+
+    public function getEmail(): ?User
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?User $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+
+}
